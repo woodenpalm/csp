7a92973ec1ddebc1a7d98a6a04321ecd87a16398
RE <turbine@UnknownHardware.localdomain>
2023-02-07 18:24:16 -0500

    Initial Commit

--- poc.js
+++ poc.js
@@ -1,3 +1,1 @@
-window.onload = function() {
-  window.location.replace('https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8');
-};
+window.location.href = 'https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8';
